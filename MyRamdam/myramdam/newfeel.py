c31e6b56f9d268098d942182e45dbe2d2f446bca
kay-apao25 <kayapao@yahoo.com>
2015-01-05 20:46:02 +0800

    added first unit test for customize feeling

---
+++ MyRamdam/myramdam/newfeel.py
@@ -0,0 +1,30 @@
+from dosql import *
+import cgi
+
+class NewFeel(object):
+    
+    def addFeel(req,newfeelname):
+        newfeelname= cgi.escape(newfeelname)
+
+        a = doSql()
+        
+        rets = x.execqry("select add_custfeel('" + newfeelname + "');",True)
+        result = []
+        
+        for ret in rets:
+            stringed = map(str, ret)
+            result.append(stringed)
+
+    def getLeanFeel(req, newfeelname):
+    	newfeelname = cgi.escape(newfeelname)
+
+    	a = doSql()
+
+    	rets = x.execqry("select get_leanfeel('" + newfeelname + "');",False)
+        result = []
+        
+        for ret in rets:
+            stringed = map(str, ret)
+            result.append(stringed)
+
+    
---
+++ MyRamdam/test/unit/customize_test.py
@@ -0,0 +1,17 @@
+import unittest
+from myramdam.newfeel import Feel
+
+class TestCustFeel(unittest.TestCase):
+    
+    def test_if_new_feeling_is_added(self):
+        feel = NewFeel("incomplete", "sad")
+        self.assertEqual(feel.addFeel(feel), "OK")
+
+    def test_feeling_leaning_towards(self):
+        feel = NewFeel("incomplete", "sad")
+        self.assertEqual(feel.add_feeling(feel), "OK")
+        self.assertEqual(feel.getLeanFeel(cfeel_id), "sad")
+
+
+
+        
